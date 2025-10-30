6158a76a594696150e2ad637e7f704a65ae01025
Marcel Betaski <aplay58.yt@gmail.com>
2025-10-30 09:45:46 +0100

    Update configs

---
+++ config/fish/config.fish
@@ -0,0 +1,28 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+function banner
+    echo
+    figlet $argv -f ~/.local/share/fonts/starwars.flf | lolcat
+    echo
+end
+
+function clock
+    tty-clock -scbt -C 7
+end
+
+function fish_greeting
+    figlet "F I S H" | lolcat
+end
+
+export PATH="$PATH:/opt/nvim-linux-x86_64/bin"
+
+export EDITOR="nvim"
+
+alias nv="nvim"
+alias ls="eza"
+alias yz="yazi"
+alias nf="clear && neofetch"
+
+eval "$(starship init fish)"
