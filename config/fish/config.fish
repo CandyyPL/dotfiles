b81e0b64f170720f0c048d9026221109a9745d5b
Marcel Betański <aplay58.yt@gmail.com>
2025-12-18 18:12:46 +0100

    update fish config

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -13,14 +13,24 @@ function clock
 end
 
 function fish_greeting
-  # figlet "F I S H" | lolcat
+    # figlet "F I S H" | lolcat
 end
+
+set -Ux FZF_DEFAULT_OPTS "\
+--color=bg+:#313244,bg:#1E1E2E,spinner:#F5E0DC,hl:#F38BA8 \
+--color=fg:#CDD6F4,header:#F38BA8,info:#CBA6F7,pointer:#F5E0DC \
+--color=marker:#B4BEFE,fg+:#CDD6F4,prompt:#CBA6F7,hl+:#F38BA8 \
+--color=selected-bg:#45475A \
+--color=border:#6C7086,label:#CDD6F4"
 
 export EDITOR="nvim"
 
-alias nv="nvim"
+alias nv='NVIM_APPNAME="nvim" nvim'
+alias lv='NVIM_APPNAME="lazyvim" nvim'
 alias ls="eza"
 alias yz="yazi"
-alias nf="clear && neofetch"
+alias lg="lazygit"
+alias gitl="git log --graph --decorate --oneline"
+alias fzf="fzf -e"
 
 eval "$(starship init fish)"
