67105c210dca1226cad9f3a34327bd47f6feb340
Marcel Betański <aplay58.yt@gmail.com>
2025-12-21 22:43:51 +0100

    rewrite install script
    get rid of linking idea, revert to copying directories

--- install.sh
+++ install.sh
@@ -2,7 +2,6 @@
 
 DEFAULT_CONFIG_DIR="$HOME/.config"
 DIRS=()
-MAX_DIR_LENGTH=0
 
 # Colors
 RED="\e[31m"
@@ -10,35 +9,8 @@ GREEN="\e[32m"
 YELLOW="\e[33m"
 RESET="\e[0m"
 
-function is_installed() {
-  local prog=$1
-
-  link_path="$DEFAULT_CONFIG_DIR/$prog"
-  config_path="config/$prog"
-
-  if [[ -L "$link_path" && "$link_path" -ef "$config_path" ]]; then
-    return 0
-  else
-    return 1
-  fi
-}
-
 function help() {
   echo "usage: ./install.sh [prog]"
-  echo
-  echo "Current dotfiles status:"
-
-  local padding=$(($MAX_DIR_LENGTH + 4))
-
-  for dir in ${DIRS[@]}; do
-    if is_installed $dir; then
-      printf "%-*s %b\n" "$padding" "$dir" "[   ${GREEN}INSTALLED${RESET}   ]"
-    else
-      printf "%-*s %b\n" "$padding" "$dir" "[ ${YELLOW}NOT INSTALLED${RESET} ]"
-    fi
-  done
-
-  echo
   echo "Install all dotfiles: ./install.sh all"
 
   exit
@@ -62,16 +34,12 @@ function install() {
   wd=$(pwd)
   prog_dir="$wd/config/$prog"
 
-  ln -s $prog_dir "$DEFAULT_CONFIG_DIR/$prog"
+  cp -r $prog_dir "$DEFAULT_CONFIG_DIR/"
 }
 
 for path in config/*; do
   dir=$(basename "$path")
   DIRS+=($dir)
-
-  if [[ ${#dir} -gt $MAX_DIR_LENGTH ]]; then
-    MAX_DIR_LENGTH=${#dir}
-  fi
 done
 
 if [[ $# -eq 0 ]]; then
@@ -90,4 +58,4 @@ for arg in "$@"; do
   install $arg
 done
 
-echo -e "[${GREEN}+${RESET}] Dotfiles linked successfully!"
+echo -e "[${GREEN}+${RESET}] Dotfiles installed successfully!"
