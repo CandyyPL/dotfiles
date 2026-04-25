4fcaf99ebe72b369a9f60dc5faa89013870cb589
CandyyPL <aplay58.yt@gmail.com>
2026-04-25 20:01:44 +0200

    update polybar config

--- config/polybar/scripts/battery.sh
+++ config/polybar/scripts/battery.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-cat /sys/class/power_supply/BAT0/capacity
+cap=$(cat /sys/class/power_supply/BAT0/capacity)
+
+if [[ $cap -gt 30 ]]; then
+  echo "%{F#74c7ec}BAT%{F-} $cap%"
+elif [[ $cap -le 30 ]] && [[ $cap -gt 10 ]]; then
+  echo "%{F#74c7ec}BAT%{F-} %{F#f2e452}$cap%%{F-}"
+
+elif [[ $cap -le 10 ]]; then
+  echo "%{F#74c7ec}BAT%{F-} %{F#eb634b}$cap%%{F-}"
+fi
+
