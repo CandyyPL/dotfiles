c0324288d39f423f9e0d807928b3de32586df9a2
CandyyPL <aplay58.yt@gmail.com>
2026-07-31 08:42:03 +0200

    change prefix color

--- config/polybar/scripts/battery.sh
+++ config/polybar/scripts/battery.sh
@@ -3,11 +3,11 @@
 cap=$(cat /sys/class/power_supply/BAT0/capacity)
 
 if [[ $cap -gt 30 ]]; then
-  echo "%{F#74c7ec}BAT%{F-} $cap%"
+  echo "%{F#60ace6}BAT%{F-} $cap%"
 elif [[ $cap -le 30 ]] && [[ $cap -gt 10 ]]; then
-  echo "%{F#74c7ec}BAT%{F-} %{F#f2e452}$cap%%{F-}"
+  echo "%{F#60ace6}BAT%{F-} %{F#f2e452}$cap%%{F-}"
 
 elif [[ $cap -le 10 ]]; then
-  echo "%{F#74c7ec}BAT%{F-} %{F#eb634b}$cap%%{F-}"
+  echo "%{F#60ace6}BAT%{F-} %{F#eb634b}$cap%%{F-}"
 fi
 
